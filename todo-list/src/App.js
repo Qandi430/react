148d52e5f7b7984db6b0c1e14810c021e687370b
Qandi <dltmdwo430@gmial.com>
2019-10-30 16:41:04 +0900

    complte todo list

---
+++ todo-list/src/App.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+import Palette from './components/Palette'
+
+const colors = ['#343a40', '#f03e3e', '#12b886', '#228ae6'];
+
+class App extends React.Component{
+
+  id = 3;
+  state = {
+    input : '',
+    todos:[
+      { id: 0, text: ' 리액트 소개', checked: false },
+      { id: 1, text: 'JSX 사용해보기', checked: true },
+      { id: 2, text: '라이프 사이클 이해하기', checked: false },
+    ],
+    color: '#343a40'
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input : e.target.value
+    })
+  }
+
+  handleCreate = () =>{
+    const {input, todos, color} = this.state;
+    this.setState({
+      input : '',
+      todos: todos.concat({
+        id : this.id++,
+        text:input,
+        checked:false,
+        color
+      })
+    })
+  }
+
+  handleKeyPress = (e) =>{
+    if(e.key === 'Enter'){
+      this.handleCreate();
+    }
+  }
+
+  handleToggle = (id) => {
+    const {todos} = this.state;
+    // 파라미터로 받은 id를 가지고 몇번째 아이템인지 검색
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index]; //선택한 객체
+    
+    // const nextTodos = [...todos]; // 배열을 복사
+
+    // //기존의 값들을 복사하고, checked 값을 덮어쓰기
+    // nextTodos[index] = {
+    //   ...selected,
+    //   checked : !selected.checked
+    // };
+
+    // this.setState({
+    //   todos : nextTodos
+    // })
+
+    this.setState({
+      todos:[
+        ...todos.slice(0,index),
+        {
+          ...selected,
+          checked: !selected.checked
+        },
+        ...todos.slice(index+1,todos.length)
+      ]
+    });
+  }
+
+  handleRemove = (id) => {
+    const {todos} = this.state;
+    this.setState({
+      todos : todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  handleSelectColor = (color) => {
+    this.setState({
+      color
+    })
+  }
+
+  render(){
+    const {input,todos,color} = this.state;
+    const{
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove,
+      handleSelectColor
+    } = this;
+
+    return(
+      <TodoListTemplate form={
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+          color={color}
+        />  
+      }
+      palette={(
+        <Palette colors={colors} seleted={color} onSelect={this.handleSelectColor}/>
+      )}
+      >
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove ={handleRemove}/>
+      </TodoListTemplate>
+    );
+  }
+}
+
+export default App;
---
+++ todo-list/src/components/Palette.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import './palette.css';
+
+const Color = ({color,active, onClick})=>{
+    return(
+        <div className={`color ${active && 'active'}`} style={{background:color}} onClick={onClick}></div>
+    )
+}
+
+const Palette = ({colors,seleted, onSelect})=>{
+    const colorList = colors.map(
+        (color) =>  (<Color color={color} active={seleted===color} onClick={()=>onSelect(color)} key={color}/>)
+    )
+    return <div className="palette">
+        {colorList}
+    </div>
+}
+export default Palette;
